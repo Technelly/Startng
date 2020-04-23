7cf749e71cd846bea80311015264989c2c26d928
Technelly <61063626+Technelly@users.noreply.github.com>
2020-04-23 08:20:27 +0100

    Update Javatask3.js

--- Javatask3.js
+++ Javatask3.js
@@ -1,7 +1,6 @@
-
 function Divisibles(count) {
- var output = [];
-var number = 1;
+  var output = [];
+  var number = 1;
  while (number <= count) {
   if (number % 2 === 0 && number % 3 === 0 && number % 5 === 0) {
     output.push("yu-gi-oh")
@@ -20,4 +19,7 @@ var number = 1;
   console.log(output);
 }
 Divisibles(100);
-Divisibles(45); 
+Divisibles(45);
+// When i saw this task, i knew its fizzBuzz but didn't know how to go about calling function twice. 
+// But thank God for google i was able to fix the error after several hours of trying. i learnt something knew.
+// i almost cried but God no shame me o.
